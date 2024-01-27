ca44d659aeaaf181ae3bf62020698f7290c00350
MUHAMAD GALIH <100691045+muhgalihhh@users.noreply.github.com>
2024-01-27 14:41:09 +0700

    add npm

---
+++ index.js
@@ -0,0 +1,5 @@
+/*
+1. Disini menggunakan inquirer npm untuk membuat prompt atau pertanyaan kepada user
+2. lalu menggunakan qr-image npm untuk mengubah inputan user berupa url menjadi qr code
+3. Membuat sebuah file txt yang berisi url yang diinputkan oleh user menggunakan native fs node module
+*/
