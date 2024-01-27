57e4b7d0c43fcac7dd296f344e0d9052f1803000
MUHAMAD GALIH <100691045+muhgalihhh@users.noreply.github.com>
2024-01-27 15:11:33 +0700

    finishing'

--- index.js
+++ index.js
@@ -3,3 +3,30 @@
 2. lalu menggunakan qr-image npm untuk mengubah inputan user berupa url menjadi qr code
 3. Membuat sebuah file txt yang berisi url yang diinputkan oleh user menggunakan native fs node module
 */
+
+import inquirer from "inquirer";
+import qr from "qr-image";
+import fs from "fs";
+
+inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "url",
+      message: "Masukkan URL : ",
+    },
+  ])
+  .then((answers) => {
+    const url = answers.url;
+    var qr_png = qr.image(url);
+    qr_png.pipe(fs.createWriteStream("qr.png"));
+    fs.writeFile("url.txt", url, (err) => {
+      if (err) {
+        console.log(err);
+      }
+      console.log("File berhasil dibuat");
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
